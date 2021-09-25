ae5460103b4d809713efdcad23d80f4a8b4e3c8a
Petr Vasilev <vahpetr@gmail.com>
2021-09-26 01:06:35 +0300

    Add login view

--- src/pages/ThunderhackPage.tsx
+++ src/pages/ThunderhackPage.tsx
@@ -29,13 +29,132 @@ import Grid from "@material-ui/core/Grid/Grid";
 import Avatar from "@material-ui/core/Avatar/Avatar";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar/ListItemAvatar";
 import { md5 } from "hash-wasm";
+import { Box, Button, Tab, Tabs } from "@material-ui/core";
+
+function a11yProps(index: number) {
+  return {
+    id: `login-tab-${index}`,
+    'aria-controls': `login-tabpanel-${index}`,
+  };
+}
+
+interface TabPanelProps {
+  children: any;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`login-tabpanel-${index}`}
+      aria-labelledby={`login-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
 function ThunderhackPage() {
   const [inn, setInn] = useState<string | null>(null);
   const [kpp, setKpp] = useState<string | null>(null);
-  const [orgType, setOrgType] = useState<string | null>(null);
+  const [orgType, setOrgType] = useState<number>(0);
+  const inputInnCustomerRef = useRef(null);
+  const inputKppCustomerRef = useRef(null);
+  const inputInnProviderRef = useRef(null);
+  const inputKppProviderRef = useRef(null);
+  const view = useState<string>("login"); // login, purchase, sale
+  // inputRef.current.value
 
-  return <div>Hello Thunderhack!</div>
+  const setOrgTypeHandler = (event: any, newValue: number) => {
+    setOrgType(newValue);
+  };
+
+  const setInnHandler = (event: any, newValue: string) => {
+    setInn(newValue);
+  };
+
+  const setKppHandler = (event: any, newValue: string) => {
+    setKpp(newValue);
+  };
+
+
+  return (
+    <Container maxWidth="sm">
+      <div>
+      <Tabs value={orgType} onChange={setOrgTypeHandler} aria-label="login tabs" centered style={{
+        paddingTop: '48px',
+        alignItems: 'center'
+      }}>
+        <Tab label="Поставщик" {...a11yProps(orgType)} />
+        <Tab label="Закупщик" {...a11yProps(orgType)} />
+      </Tabs>
+      <TabPanel value={orgType} index={0}>
+        <div>
+        <Box sx={{
+          display: 'flex',
+          alignItems: 'center',
+        }} style={{
+          justifyContent: 'center'
+        }}>
+          <TextField id="inn-customer" label="ИНН" variant="outlined" style={{
+            paddingRight: '16px',
+          }} innerRef={inputInnCustomerRef} />
+          <TextField id="kpp-customer" label="КПП" variant="outlined" innerRef={inputKppCustomerRef} style={{
+            paddingRight: '16px',
+          }} />
+        </Box>
+        <Box sx={{
+          display: 'flex',
+          alignItems: 'center',
+          paddingTop: '24px'
+        }} style={{
+          justifyContent: 'center'
+        }}>
+          <Button variant="contained" color="primary" size="large">
+            Войти
+          </Button>
+        </Box>
+        </div>
+      </TabPanel>
+      <TabPanel value={orgType} index={1}>
+        <Box sx={{
+          display: 'flex',
+          alignItems: 'center',
+        }} style={{
+          justifyContent: 'center'
+        }}>
+          <TextField id="inn-provider" label="ИНН" variant="outlined" style={{
+            paddingRight: '16px',
+          }} innerRef={inputInnProviderRef} />
+          <TextField id="kpp-provider" label="КПП" variant="outlined" innerRef={inputKppProviderRef} style={{
+            paddingRight: '16px',
+          }} />
+        </Box>
+        <Box sx={{
+          display: 'flex',
+          alignItems: 'center',
+          paddingTop: '24px'
+        }} style={{
+          justifyContent: 'center'
+        }}>
+          <Button variant="contained" color="primary" size="large">
+            Войти
+          </Button>
+        </Box>
+      </TabPanel>
+      </div>
+    </Container>
+  );
 }
 
 export default ThunderhackPage;
